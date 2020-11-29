eb10dc0d3aafd48d695aad05733632e49e845aac
lihaifeng <lihaifeng@wxm.com>
2020-11-29 14:53:54 +0800

    Update wxpay.nim

--- src/wxpay.nim
+++ src/wxpay.nim
@@ -1,4 +1,4 @@
-﻿## 微信支付API接口模块，用户最终的微信支付相关的调用都应该调用此模块的相关函数
+## 微信支付API接口模块，用户最终的微信支付相关的调用都应该调用此模块的相关函数
 ## 刷卡支付实现,实现了一个刷卡支付的流程，流程如下：
 ## 1、提交刷卡支付,
 ## 2、根据返回结果决定是否需要查询订单，如果查询之后订单还未变则需要返回查询（一般反复查10次）,
@@ -58,9 +58,9 @@ proc cancel(config: WxPayData, outTradeNo: string,
     return false
 
 {.push stdcall dynlib exportc.}
-proc wxMicropay*(input, config: WxPayData,
-                 succFlag: var bool): WxPayData =
-  ## 付款码支付，返回查询接口的结果
+proc wxMicropay*(input, config: WxPayData, results: var WxPayData): bool =
+  ## 付款码支付，input是用户请求数据，config是用户配置数据，
+  ## results是接口返回的结果，本函数返回是否成功true/false
   ## 此函数将要导出到lib，供其他语言调用
   # 1、提交被扫支付
   var ret = micropay(input, config, 5)
@@ -68,13 +68,13 @@ proc wxMicropay*(input, config: WxPayData,
   if not ret.hasKey("return_code"):
     raise newException(WxPayException, "支付接口调用失败！")
   if ret["return_code"] == "FAIL":
-    succFlag = false
-    return ret
+    results = ret
+    return false
   # 2、接口调用成功，明确返回调用失败
   if ret["return_code"] == "SUCCESS" and ret["result_code"] == "FAIL" and
      ret["err_code"] != "USERPAYING" and ret["err_code"] != "SYSTEMERROR":
-    succFlag = false
-    return ret
+    results = ret
+    return false
   # 3、确认支付是否成功
   var queryTimes = 10
   var succResult = 0
@@ -88,20 +88,21 @@ proc wxMicropay*(input, config: WxPayData,
       sleep(2000)
       continue
     elif succResult == 1: # 查询成功
-      succFlag = true
-      return ret
+      results = ret
+      return true
     else: # 订单交易失败
       break
     dec(queryTimes)
   # 4、10次确认失败，则撤销订单
   if not cancel(config, outTradeNo):
     raise newException(WxpayException, "撤销单失败！")
-  succFlag = false # 到这里说明支付不成功
-  return queryResult
+  # 到这里说明支付不成功
+  results = queryResult
+  return false
 
-proc wxRefund*(input, config: WxPayData,
-               succFlag: var bool): WxPayData =
-  ## 退款申请，返回申请受理结果
+proc wxRefund*(input, config: WxPayData, results: var WxPayData): bool =
+  ## 退款申请，input是用户请求数据，config是用户配置数据，
+  ## results是接口返回的结果，本函数返回是否成功true/false
   ## 此函数将要导出到lib，供其他语言调用
   # 调用申请退款
   var ret = refund(input, config)
@@ -109,14 +110,15 @@ proc wxRefund*(input, config: WxPayData,
   if not ret.hasKey("return_code"):
     raise newException(WxPayException, "退款申请接口调用失败！")
   if ret["return_code"] == "FAIL":
-    succFlag = false
-    return ret
+    results = ret
+    return false
   # 调用成功
   if ret["return_code"] == "SUCCESS" and ret["result_code"] == "SUCCESS":
-    succFlag = true
+    results = ret
+    return true
   else:
-    succFlag = false
-  result = ret
+    results = ret
+    return false
 
 proc wxErrorMessage*(): string =
   result = errorMessage()
