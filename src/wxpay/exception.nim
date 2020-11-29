3c0bee2fc9316be1631ed4d0b4ed1709410443e8
lihaifeng <lihaifeng@wxm.com>
2020-11-29 12:22:56 +0800

    Delete exception.nim

--- src/wxpay/exception.nim
+++
@@ -1,5 +0,0 @@
-type WxPayException* = system.Exception
-  ## 微信支付API异常处理类
-
-proc errorMessage*(): string =
-  result = "获取到异常 " & repr(getCurrentException()) & " 消息： " & getCurrentExceptionMsg()  
