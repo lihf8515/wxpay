cf45c5b45794c09ea166367cccd5283c5c7d6cce
lihaifeng <lihaifeng@wxm.com>
2020-11-29 14:56:09 +0800

    Update test1.nim

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,8 +1,10 @@
 import tables
 import wxpay
 
-var succFlag: bool
-var input, config = OrderedTable[string, string]()
+var results = OrderedTable[string, string]()
+var input = OrderedTable[string, string]()
+var config = OrderedTable[string, string]()
+
 config["appid"] = ""
 config["appsecret"] = ""
 config["key"] = ""
@@ -11,8 +13,8 @@ config["notify_url"] = ""
 config["proxy_host"] = "0.0.0.0"
 config["proxy_port"] = "0"
 config["report_level"] = "1"
-config["sslcert_path"] = ""
-config["sslkey_path"] = ""
+config["sslcert_path"] = "cert/apiclient_cert.pem"
+config["sslkey_path"] = "cert/apiclient_key.pem"
 config["sign_type"] = "HMAC-SHA256"
 
 input["auth_code"] = "134728953980228598"
@@ -21,12 +23,28 @@ input["total_fee"] = "1"
 input["out_trade_no"] = "test00000001"
 
 try:
-  var ret = wxMicropay(input, config, succFlag)
-  if succFlag:
+  if wxMicropay(input, config, results):
     echo "支付成功"
-    echo ret
+    echo results
   else:
     echo "支付失败"
-    echo ret
+    echo results
+except:
+  echo wxErrorMessage()
+
+results.clear()
+input.clear()
+input["out_refund_no"] = "132564989589668"
+input["out_trade_no"] = "test00000001"
+input["refund_fee"] = "1"
+input["total_fee"] = "1"
+
+try:
+  if wxRefund(input, config, results):
+    echo "请求退款成功"
+    echo results
+  else:
+    echo "请求退款失败"
+    echo results
 except:
   echo wxErrorMessage()
