250100eb2ccbd7ab4f35c197b7783ba4d2b13d6e
lihaifeng <lihaifeng@wxm.com>
2020-12-07 21:49:36 +0800

    Update exception.nim

--- src/wxpay/exception.nim
+++ src/wxpay/exception.nim
@@ -6,7 +6,8 @@ proc errorMessage*(): string =
   ## 如果不是Debug编译模式则不显示堆栈跟踪信息。
   var err = "异常信息：" & getCurrentExceptionMsg()
   when not defined(release) and not defined(danger):
-    err.add("\n跟踪信息：")
-    for item in getCurrentException().getStackTraceEntries:
-      err.add($item & "\n\n")
+    err.add("\n跟踪信息：\n")
+    for i, item in getCurrentException().getStackTraceEntries:
+      err.add($(i + 1) & "、在 " & $item.filename & " 文件的 " & $item.procname &
+              " 函数第 " & $item.line & " 行。\n")
   result = err
