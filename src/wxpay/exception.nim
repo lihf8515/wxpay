b9b8d8946c67bf89af61a6218b827a444bd1c540
lihaifeng <lihaifeng@wxm.com>
2020-12-07 20:11:47 +0800

    Update exception.nim

--- src/wxpay/exception.nim
+++ src/wxpay/exception.nim
@@ -2,7 +2,11 @@ type WxPayException* = system.Exception
   ## 微信支付API异常处理类
 
 proc errorMessage*(): string =
-  var err = "异常信息：" & getCurrentExceptionMsg() & "\n跟踪信息："
-  for item in getCurrentException().getStackTraceEntries:
-    err.add($item & "\n\n")
+  ## 显示出错信息，如果是Debug编译模式则显示堆栈跟踪信息，
+  ## 如果不是Debug编译模式则不显示堆栈跟踪信息。
+  var err = "异常信息：" & getCurrentExceptionMsg()
+  when not defined(release) and not defined(danger):
+    err.add("\n跟踪信息：")
+    for item in getCurrentException().getStackTraceEntries:
+      err.add($item & "\n\n")
   result = err
