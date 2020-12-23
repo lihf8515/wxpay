4d9d2a4ab50d617ff5c5265719368c7172aee2c6
lihaifeng <lihaifeng@wxm.com>
2020-12-23 14:23:38 +0800

    Update downloadbill.nim

--- src/wxpay/downloadbill.nim
+++ src/wxpay/downloadbill.nim
@@ -6,7 +6,7 @@
 
 ## 下载对账单模块
 
-import tables
+import tables, strutils
 
 import private/utils
 import exception
@@ -16,15 +16,15 @@ proc downloadBill*(input: var WxPayData, config: WxPayData,
   ## 下载对账单，input参数中bill_date为必填参数,
   ## appid、mchid由config参数携带，nonce_str由系统自动填入,
   # 检测必填参数
-  if not input.hasKey("bill_date"):
+  if not input.hasKey("bill_date") or strip(input["bill_date"]) == "":
     raise newException(WxPayException, "对账单接口中，缺少必填参数bill_date！")
   # 初始化并返回有效的配置数据
   let configData = initConfig(config, typeDownloadBill)
   # 初始化订单查询请求数据
   var inputData = WxPayData()
   if input.hasKey("bill_type"):
-    inputData["bill_type"] = input["bill_type"]
-  inputData["bill_date"] = input["bill_date"]
+    inputData["bill_type"] = strip(input["bill_type"])
+  inputData["bill_date"] = strip(input["bill_date"])
   inputData["appid"] = configData["appid"] # 设置公众账号ID
   inputData["mch_id"] = configData["mch_id"] # 设置商户号
   inputData["nonce_str"] = getNonceStr() # 设置随机字符串
@@ -36,4 +36,3 @@ proc downloadBill*(input: var WxPayData, config: WxPayData,
     result = ""
   else:
     result = response
-
