d63b4cf19a658a8c12f388c18215e690a8db28fc
lihaifeng <lihaifeng@wxm.com>
2020-12-23 19:44:18 +0800

    Update reverse.nim

--- src/wxpay/reverse.nim
+++ src/wxpay/reverse.nim
@@ -6,7 +6,7 @@
 
 ## 撤销订单模块
 
-import tables, strutils
+import tables
 
 import private/utils
 import exception
@@ -20,17 +20,17 @@ proc reverse*(input: var WxPayData, config: WxPayData,
   # 检测必填参数
   if (not input.hasKey("out_trade_no") and
       not input.hasKey("transaction_id")) or
-     (strip(input["out_trade_no"]) == "" and
-      strip(input["transaction_id"]) == ""):
+     (input["out_trade_no"] == "" and
+      input["transaction_id"] == ""):
     raise newException(WxPayException, "撤销订单接口，参数out_trade_no和transaction_id必须填写一个！")
   # 初始化并返回有效的配置数据
   let configData = initConfig(config, typeReverse)
   # 初始化撤销订单请求数据
   var inputData = WxPayData()
   if input.hasKey("out_trade_no"):
-    inputData["out_trade_no"] = strip(input["out_trade_no"])
+    inputData["out_trade_no"] = input["out_trade_no"]
   if input.hasKey("transaction_id"):
-    inputData["transaction_id"] = strip(input["transaction_id"])
+    inputData["transaction_id"] = input["transaction_id"]
   inputData["appid"] = configData["appid"] # 设置公众账号ID
   inputData["mch_id"] = configData["mch_id"] # 设置商户号
   inputData["nonce_str"] = getNonceStr() # 设置随机字符串
