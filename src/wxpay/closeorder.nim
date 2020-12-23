8ed72fc23a4f21fba97dbd9cb4754a514450dcc4
lihaifeng <lihaifeng@wxm.com>
2020-12-23 19:33:29 +0800

    Update closeorder.nim

--- src/wxpay/closeorder.nim
+++ src/wxpay/closeorder.nim
@@ -6,7 +6,7 @@
 
 ## 关闭订单模块
 
-import tables, strutils
+import tables
 
 import private/utils
 import exception
@@ -17,13 +17,13 @@ proc closeOrder*(input: var WxPayData, config: WxPayData,
   ## 关闭订单，input参数中out_trade_no必填,
   ## appid、mchid由config参数携带，nonce_str由系统自动填入,
   # 检测必填参数
-  if not input.hasKey("out_trade_no") or strip(input["out_trade_no"]) == "":
+  if not input.hasKey("out_trade_no") or input["out_trade_no"] == "":
     raise newException(WxPayException, "关闭订单接口中，out_trade_no必填！")
   # 初始化并返回有效的配置数据
   let configData = initConfig(config, typeCloseOrder)
   # 初始化订单查询请求数据
   var inputData = WxPayData()
-  inputData["out_trade_no"] = strip(input["out_trade_no"])
+  inputData["out_trade_no"] = input["out_trade_no"]
   inputData["appid"] = configData["appid"] # 设置公众账号ID
   inputData["mch_id"] = configData["mch_id"] # 设置商户号
   inputData["nonce_str"] = getNonceStr() # 设置随机字符串
@@ -35,3 +35,4 @@ proc closeOrder*(input: var WxPayData, config: WxPayData,
   let ret = checkResults(configData, response) # 检测接口调用返回的xml字符串是否合法，并且将其转换为有序表返回
   reportCostTime(configData, url, startTimeStamp, ret) # 上报请求花费时间
   result = ret
+
